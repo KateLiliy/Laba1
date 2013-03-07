8d673d0a810107008386ea30e47946d2f475d925
Ekaterina Derbina <kateliliy@mail.ru>
2013-03-07 18:48:51 +0400

    commit

--- Pyzirok.cpp
+++ Pyzirok.cpp
@@ -1,42 +1,45 @@
-# include <cstdio>
-# include <cstdlib>
-# include <cmath>
-
-
-void print_mas(int n,int mas[])  // процедура, которая выводит массив
-{
-	for (int i=1; i<=n; i++)
-		printf("%d ",mas[i]);
-
-}
-inline void Swap(int &a, int &b)
-{
-    int buf = a;
-    a = b;
-    b = buf;
-}
-void sort(int n,int mas[])  // сама сортировка
-{
-	for (int i = 1; i < n; i++)
-		for (int j = 1; j < n; j++)
-			if (mas[j]>mas[j+1])
-			    Swap(mas[j],mas[j+1]);   
-}
-
-
+#include "stdafx.h"
+#include <iostream>
+ 
+using namespace std;
+ 
+ 
 int main()
 {
-	int n;        
-           int mas[1000];
-	
-	scanf("%d",&n);
-
-	for (int i=1 ;i <= n; i++)
-		scanf("%d",&mas[i]);
-	
-           sort(n, mas);
-
-	print_mas(n,mas);
-
-	return(0);
+ 
+int a[6];
+int temp;
+a[1] = 2;
+a[2] = 5;
+a[3] = 7;
+a[4] = 2;
+a[5] = 1;
+for (int i = 1; i<6; i++)
+{
+cout << a[i];
+}
+for (int i = 1; i<6; i++)
+{
+for (int j = 1; j<6; j++)
+{
+if (a[i]>a[j])
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+ 
+}
+}
+}
+cout << endl;
+for (int i = 1; i<6; i++)
+{
+cout << a[i];
+}
+ 
+ 
+return 0;
+}
+ 
+ 
 }
